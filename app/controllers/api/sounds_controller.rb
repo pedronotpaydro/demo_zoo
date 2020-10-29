607c72eadf3c1fb318fb2b0f81433bac758e120c
Pedro <orsomethingelse@gmail.com>
2020-10-29 17:45:08 -0500

    if conditional almost working

--- app/controllers/api/sounds_controller.rb
+++ app/controllers/api/sounds_controller.rb
@@ -1,7 +1,20 @@
 class Api::SoundsController < ApplicationController
   def all_caps
     input_value = params["sound"]
-    @output_value = "Your sound in all caps is #{input_value.upcase}"
+
+    if input_value == "moo"
+      @output_value = "The cow goes
+      #{input_value}"
+    elsif input_value == "caw"
+      @output_value = "The crow goes
+      #{input_value}"
+    elsif input_value == "rawr"
+      @output_value = "The trex goes
+      #{input_value}"
+    else
+      @output_value = "Your sound in all caps is #{input_value.upcase}"
+    end
+
     render "all_caps_sound.json.jb"
   end
 end
