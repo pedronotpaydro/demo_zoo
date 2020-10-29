933d04377658f6453e44b022dfe62dd25d102d76
Pedro <orsomethingelse@gmail.com>
2020-10-29 17:23:34 -0500

    added single and all animal routes/views

--- app/controllers/api/animals_controller.rb
+++ app/controllers/api/animals_controller.rb
@@ -1,2 +1,11 @@
 class Api::AnimalsController < ApplicationController
+  def first_animal_action
+    @first_animal = Animal.first
+    render "first.json.jb"
+  end
+
+  def all_animal_action
+    @animals = Animal.all
+    render "all.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/animals" => "animals#first_animal_action"
+    get "/all_animals" => "animals#all_animal_action"
+  end
 end
